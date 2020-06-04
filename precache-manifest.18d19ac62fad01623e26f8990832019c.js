d2f7be989d936dd9fffe0c9210a56bfebfe3e57a
Senya Volkov <formobileprima@gmail.com>
2020-06-04 20:04:01 +0300

    Updates

---
+++ precache-manifest.18d19ac62fad01623e26f8990832019c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69060b33fbbaa44af0e19d0f9e82a303",
+    "url": "/Intello-gate/index.html"
+  },
+  {
+    "revision": "87a9f3ca2a6489d2b13b",
+    "url": "/Intello-gate/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "3843e87b5193deb9e1a8",
+    "url": "/Intello-gate/static/css/main.96961807.chunk.css"
+  },
+  {
+    "revision": "87a9f3ca2a6489d2b13b",
+    "url": "/Intello-gate/static/js/2.411fcdd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Intello-gate/static/js/2.411fcdd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3843e87b5193deb9e1a8",
+    "url": "/Intello-gate/static/js/main.32167e7f.chunk.js"
+  },
+  {
+    "revision": "9ae09a057d223b8d1d3a",
+    "url": "/Intello-gate/static/js/runtime-main.8330a1c6.js"
+  }
+]);
